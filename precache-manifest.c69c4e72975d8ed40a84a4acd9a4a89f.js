0f2aee0133b3ccde7ea361c2c196b45a5d71f509
akito <4kito.nakano@gmail.com>
2020-02-23 16:37:19 +0900

    Updates

---
+++ precache-manifest.c69c4e72975d8ed40a84a4acd9a4a89f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c010cbfc7ed2feb3bcc4057ef3fd4aed",
+    "url": "/3DViewer/index.html"
+  },
+  {
+    "revision": "ebc4e89292f4ff98587c",
+    "url": "/3DViewer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "41149a46b865a8ae4879",
+    "url": "/3DViewer/static/js/2.19b19659.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/3DViewer/static/js/2.19b19659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebc4e89292f4ff98587c",
+    "url": "/3DViewer/static/js/main.ae0670c5.chunk.js"
+  },
+  {
+    "revision": "2a4d61016b7faf2e1446",
+    "url": "/3DViewer/static/js/runtime-main.071306a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/3DViewer/static/media/logo.5d5d9eef.svg"
+  }
+]);
