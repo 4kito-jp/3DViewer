3d44337b294fa54c5c5f0a6994aebff6f3061ce1
akito <4kito.nakano@gmail.com>
2020-03-23 01:06:58 +0900

    Updates

---
+++ precache-manifest.620bc9ff6abea60ff0e53059654e4c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "817c27f13cbc1f4f7bb03842d1a79e5d",
+    "url": "/3DViewer/index.html"
+  },
+  {
+    "revision": "229e47da71f06e5e19e8",
+    "url": "/3DViewer/static/css/main.997fc1e3.chunk.css"
+  },
+  {
+    "revision": "2cb8c6422d71102e8121",
+    "url": "/3DViewer/static/js/2.98dde89e.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/3DViewer/static/js/2.98dde89e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229e47da71f06e5e19e8",
+    "url": "/3DViewer/static/js/main.2aeb83b2.chunk.js"
+  },
+  {
+    "revision": "2a4d61016b7faf2e1446",
+    "url": "/3DViewer/static/js/runtime-main.071306a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/3DViewer/precache-manifest.963270368d89eff83a398a6c9c9c8a00.js"
+  "/3DViewer/precache-manifest.620bc9ff6abea60ff0e53059654e4c58.js"
 );
 
 self.addEventListener('message', (event) => {
